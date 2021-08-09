528bb02f3b102909d02da803ffade0fb16de15dd
cheahaunyik <cheahaunyik@gmail.com>
2021-08-09 14:37:26 +0800

    Add project files.

---
+++ 1. Two Sum/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _1._Two_Sum
+{
+    class Program
+    {
+        //https://leetcode.com/problems/two-sum/
+
+        static void Main(string[] args)
+        {
+            var problem = new int[2] { 3, 3 };
+            var result = TwoSum(problem, 6);
+        }
+
+        static int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i <= nums.Length; i++)
+            {
+                int compareIdx = i;
+
+                while (++compareIdx < nums.Length)
+                {
+                    if (nums[i] + nums[compareIdx] == target)
+                    {
+                        return new int[2] { i, compareIdx };
+                    }
+                }
+            }
+            return new int[2] { 0, 0 };
+        }
+
+    }
+}
---
+++ 2. Add Two Numbers/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace _2._Add_Two_Numbers
+{
+    class Program
+    {
+        //https://leetcode.com/problems/add-two-numbers/
+
+        static void Main(string[] args)
+        {
+            Solution sln = new Solution();
+
+
+            ListNode l1 = sln.GenerateListNode(1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1);
+            ListNode l2 = sln.GenerateListNode(5, 6, 4);
+
+            var res = sln.AddTwoNumbers(l1, l2);
+        }
+    }
+
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Solution
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            string s1 = "";
+            while (true)
+            {
+                s1 = l1.val + s1;
+                if (l1.next is null) break;
+                l1 = l1.next;
+            }
+
+            string s2 = "";
+            while (true)
+            {
+                s2 = l2.val + s2;
+                if (l2.next is null) break;
+                l2 = l2.next;
+            }
+
+            var res = (System.Numerics.BigInteger.Parse(s1) + System.Numerics.BigInteger.Parse(s2)).ToString();
+
+            ListNode ln = null;
+
+            for (int i = 0; i < res.Length; i++)
+            {
+                ln = new ListNode(int.Parse(res[i].ToString()), ln);
+            }
+
+            return ln;
+        }
+
+        public ListNode GenerateListNode(params int[] arr)
+        {
+            ListNode ln = null;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                ln = new ListNode(arr[i], ln);
+            }
+
+            return ln;
+        }
+    }
+}
