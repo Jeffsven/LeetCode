3e6df821a99be9a6b392735e12b8d6a6ebf7567a
cheahaunyik <cheahaunyik@gmail.com>
2023-01-03 10:12:51 +0800

    update > 944. Delete Columns to Make Sorted

---
+++ 944. Delete Columns to Make Sorted/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _944._Delete_Columns_to_Make_Sorted
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sArr = { "", "", "" };
+            Console.WriteLine(new Solution().MinDeletionSize(sArr));
+        }
+    }
+
+    public class Solution
+    {
+        public int MinDeletionSize(string[] strs)
+        {
+            int resCount = 0;
+
+            for (int i = 0; i < strs[0].Length; i++)
+            {
+                string s = "";
+                for (int j = 0; j < strs.Length; j++)
+                {
+                    s += $"{strs[j][i]}";
+                }
+                var sorteds = string.Join("", s.OrderBy(x => x));
+                if (s != sorteds) resCount++;
+            }
+            return resCount;
+        }
+    }
+}
